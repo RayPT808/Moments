d366df39fdaa082ac482349b65d62a9247445da6
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-12-06 16:09:07 +0000

    Added sign up button and styling.

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://djangorestframework1-47e5ef4ee651.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
