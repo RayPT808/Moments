d867278af630eb8876f1b600c6c92381be8d952e
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-12-12 14:29:14 +0000

    Navbar modified.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://djangorestframework1-47e5ef4ee651.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
